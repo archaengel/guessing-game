35de0bd788e10d6de39c372764585804395692b5
Archaengel <god11341258@gmail.com>
2020-09-02 01:02:27 -0700

    feat: Retry on invalid input

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret numbder is: {}", secret_number);
-
-
     loop {
         println!("Please input your guess.");
 
@@ -19,7 +16,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Failed to parse integer");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }
 
         println!("You guessed: {}", guess);
 
