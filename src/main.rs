5da0ff9b3052edf9fb0f06378b00dae6999a6eac
Archaengel <god11341258@gmail.com>
2020-09-02 00:58:21 -0700

    feat: Loop until user guesses correctly

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,28 @@ fn main() {
 
     println!("The secret numbder is: {}", secret_number);
 
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Failed to parse integer");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Failed to parse integer");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 
 }
